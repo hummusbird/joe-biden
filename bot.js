aba2243baae5bb1d12d391861ec0a55369d3a072
hummusbird <birb@birb.cc>
2023-04-05 09:06:04 +0100

    remove nonsense

--- bot.js
+++ bot.js
@@ -62,10 +62,6 @@ await client.on('message', async message => {
 
     var response = "";
     var fullresponse = ""
-    var result = ""
-
-    delay(500);
-    socket.emit("stop")
 
     socket.on("result", result => {
         var i = 0;
